9a270c5f01f883117b2050f9a2d5ec9e94c053ff
NachoConort <nachoconort@gmail.com>
2022-11-09 10:57:59 -0300

    Added styles

---
+++ src/styles/RegisterStyles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#EDEDEE',
+    flex: 1,
+  },
+  elipseImage: {
+    width: 250,
+    height: 250,
+    top: -80,
+    left: -50,
+  },
+});
+
+export default styles;
