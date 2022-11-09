86e74fb94bf8d859804945e6d6142ea34a25e5db
NachoConort <nachoconort@gmail.com>
2022-11-09 13:33:45 -0300

    Terminada la vista. Falta funcionalidad

--- src/components/views/Register.js
+++ src/components/views/Register.js
@@ -1,7 +1,10 @@
-import { View, Image } from "react-native";
-
+import { View, Image, Text } from "react-native";
 // ESTILOS
 import styles from "../../styles/RegisterStyles";
+// INPUTS
+import RegisterInputs from "../inputs/RegisterInputs";
+// BUTTON
+import RegisterButton from "../buttons/RegisterButton";
 
 const Register = () => {
   return (
@@ -9,6 +12,13 @@ const Register = () => {
       <Image
       source={require('../../assets/images/elipse.png')} 
       style={styles.elipseImage}/>
+      <View style={styles.viewTexts}>
+        <Text style={styles.welcome}>Welcome Onboard!</Text>
+        <Text style={styles.smalltext}>Let's help you meet up your tasks.</Text>        
+      </View>
+      <RegisterInputs/>
+      <RegisterButton/>
+      <Text></Text> 
     </View>
   )
 };
