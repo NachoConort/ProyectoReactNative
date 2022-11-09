5a6f637b3adabd66430e03e2b2c5aa5be2ff30fc
NachoConort <nachoconort@gmail.com>
2022-11-09 13:34:36 -0300

    Nuevos estilos al componente

--- src/styles/RegisterStyles.js
+++ src/styles/RegisterStyles.js
@@ -11,6 +11,51 @@ const styles = StyleSheet.create({
     top: -80,
     left: -50,
   },
+  viewTexts: {
+    marginTop: -40,
+  },
+  welcome: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+    color: 'black',
+  },
+  smalltext: {
+    fontSize: 15,
+    color: 'black',
+    textAlign: 'center',
+    marginTop: 20
+  },
+  viewinputs: {
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  inputregister: {
+    width: '90%',
+    height: 50,
+    borderRadius: 30,
+    marginVertical: 10,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: 'white',
+  },
+  viewButton: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  buttonRegister: {
+    width: '90%',
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#50C2C9',
+    justifyContent: 'center',
+  },
+  textButton: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
 });
 
 export default styles;
