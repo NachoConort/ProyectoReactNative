de4f48aefe8ac988e9e26466023ea75d165ffff9
NachoConort <nachoconort@gmail.com>
2022-11-09 09:47:32 -0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, backgroundColor: 'blue'}}>
+      <View style={{width: 40, height: 40, backgroundColor: 'red'}}></View>
+    </View>
+  )
+};
+
+export default App;
