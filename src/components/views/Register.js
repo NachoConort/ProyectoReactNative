14fa72265edd7731ccdda19e37ccecd68be43b4b
NachoConort <nachoconort@gmail.com>
2022-11-09 10:59:33 -0300

    New Register and Login components

--- src/components/views/Register.js
+++ src/components/views/Register.js
@@ -1,5 +1,16 @@
-const Register = () => {
+import { View, Image } from "react-native";
 
+// ESTILOS
+import styles from "../../styles/RegisterStyles";
+
+const Register = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+      source={require('../../assets/images/elipse.png')} 
+      style={styles.elipseImage}/>
+    </View>
+  )
 };
 
 export default Register;
