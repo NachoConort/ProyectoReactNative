fc1327944e6251705fcb3a92803eaadccf16be91
NachoConort <nachoconort@gmail.com>
2022-11-11 10:11:27 -0300

    Nuevo componente boton

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { View } from 'react-native';
 import Register from './src/components/views/Register';
 import Login from './src/components/views/Login';
 // IMPORTAR COMPONENTES
+
 const App = () => {
   return (
    // <Register/>
