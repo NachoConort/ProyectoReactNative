fb5964fbcff9b89380ded82c05af09b0cc84a97c
NachoConort <nachoconort@gmail.com>
2022-11-11 18:51:24 -0300

    Creadas rutas del Navigation

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -4,7 +4,14 @@ import styles from "../../styles/HomeStyles";
 // BUTTON
 import ButtonRyL from "../buttons/ButtonRyL";
 
-const Home = () => {
+const Home = (props) => {
+
+  const { navigation } = props;
+
+  const goRegister = () => {
+    navigation.navigate('Register');
+  };
+
   return (
     <View style={styles.container}>
       <Image
@@ -17,7 +24,9 @@ const Home = () => {
       <View style={styles.viewText}>
         <Text style={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Magna un volupat, tristique lacinia ut. Elementum non turpis nullam ipsum</Text>
       </View>
-      <ButtonRyL text='Get Started'/>
+      <ButtonRyL 
+      text='Get Started' 
+      onPress={goRegister} />
     </View>
   );
 };
